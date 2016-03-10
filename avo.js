d8882ae522db863e2ea47c33b11d5d5a9f4905b3
Saffron <saffronbeatson@gmail.com>
2016-03-10 09:42:02 +0200

    working up until cheapest deal

--- avo.js
+++ avo.js
@@ -1,8 +1,35 @@
 //convert the string into an array (split out the quantity and the price);
-module.exports = function(string) {
+exports.avoArray = function(string) {
   var array = string.split(" for R").join();
   var secondArray = array.split(",");
   for (var i = 0; i < array.length; i++) {}
   return (secondArray)
 
+
 };
+
+//take an array as an input and calculate the Price Per Avo for each deal (make sure you use just two decimal points);
+exports.calculate = function(arrayarray) {
+  var priceArray = [];
+
+  var price1 = (arrayarray[1] / arrayarray[0]).toFixed(2);
+  var price2 = (arrayarray[3] / arrayarray[2]).toFixed(2);
+  var price3 = (arrayarray[5] / arrayarray[4]).toFixed(2);
+  var price4 = (arrayarray[7] / arrayarray[6]).toFixed(2);
+
+  priceArray.push(price1, price2, price3, price4);
+
+  return (priceArray);
+
+
+}
+//find the cheapest deal;
+
+exports.cheapest = function (){
+
+
+  Math.min.apply (null, priceArray);
+
+  return (bestDeal);
+
+}
--- test/avo_test.js
+++ test/avo_test.js
@@ -1,15 +1,25 @@
 var assert = require("assert");
 var avo = require('../avo');
 
-describe("produce an array", function() {
+describe("For the bestdeal kata,", function() {
 
-  it('from a string', function() {
+  it('I should create a price array', function() {
 
-    var result = avo('1 for R3, 2 for R7, 3 for R10, 5 for R14.50');
+    var result = avo.avoArray('1 for R3, 2 for R7, 3 for R10, 5 for R14.50');
     assert.deepEqual(result, [1, 3, 2, 7, 3, 10, 5, 14.50]);
-
 
   });
 
+  it('I should calculate the price per avo for each deal', function() {
+    var result = avo.calculate([1, 3, 2, 7, 3, 10, 5, 14.50]);
+    assert.deepEqual(result, [3, 3.50, 3.33, 2.90])
+
+  });
+
+ it ('I should find the cheapest deal', function(){
+   var result = avo.cheapest([3, 3.50, 3.33, 2.90]);
+   assert.deepEqual(result, [2.90])
+
+ });
 
 });
